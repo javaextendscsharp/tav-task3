a365e58906869d2125ffa15a8103c23f74f055a7
Aleksandar <es23m010@technikum-wien.at>
2024-09-30 21:40:15 +0200

    gcovr report generated

--- src/area.c
+++ src/area.c
@@ -1,19 +1,28 @@
 #include <math.h>
 
-float area(char type, float a, float b, float c) {
+float area(char type, float a, float b, float c) 
+{
   float res;
-  if (type == 'c') {
+  if (type == 'c') 
+  {
     res = pow(a, 2) * M_PI;
     return res;
-  } else if (type == 's') {
+  } 
+  else if (type == 's') 
+  {
     res = a * a;
     return res;
-  } else if (type == 'r') {
+  } 
+  else if (type == 'r') 
+  {
     res = a * b;
     return res;
-  } else if (type == 't') {
+  } 
+  else if (type == 't') 
+  {
     res = (a + b + c) / 2;
     return sqrt(res * (res - a) * (res - b) * (res - c));
-  } else
+  } 
+  else
     return -1.0;
 }
--- src/testarea.c
+++ src/testarea.c
@@ -3,7 +3,8 @@
 #include <math.h>
 #include <stdio.h>
 
-int main(void) {
+int main(void) 
+{
   assert(area('c', 1, 0, 0) - M_PI < 0.0001);
   assert(area('s', 1, 0, 0) - 1.0 < 0.0001);
   assert(area('t', 1, 1, 1) - 1.5 < 0.0001);
